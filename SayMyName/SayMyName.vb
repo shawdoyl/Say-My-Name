7ed3d1c2403bc981efc02b4170a063bc26ba8e3f
Doyle Shaw <shawdoyl@isu.edu>
2020-08-26 08:17:20 -0600

    First Commit/ Code to ask for and display name

---
+++ SayMyName/SayMyName.vb
@@ -0,0 +1,22 @@
+﻿'Doyle_Shaw
+'RCET0265
+'Spring 2020
+'Say My Name
+'https://github.com/shawdoyl/Say-My-Name
+
+Option Explicit On
+Option Strict On
+
+Module SayMyName
+
+    Sub Main()
+
+        Dim userMessage As String
+        Console.WriteLine("Please enter your name. Press Enter when done.")
+        userMessage = Console.ReadLine()
+        Console.WriteLine("You entered " & userMessage & ", welcome " & userMessage & ".")
+        Console.ReadLine()
+
+    End Sub
+
+End Module
