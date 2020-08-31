5d226c89615c0a92d64247b83c1aa6e09b3db6ba
Doyle Shaw <shawdoyl@isu.edu>
2020-08-31 08:33:20 -0600

    Add multiple options for names

--- SayMyName/SayMyName.vb
+++ SayMyName/SayMyName.vb
@@ -1,6 +1,6 @@
 ﻿'Doyle_Shaw
 'RCET0265
-'Spring 2020
+'Fall 2020
 'Say My Name
 'https://github.com/shawdoyl/Say-My-Name
 
@@ -14,7 +14,17 @@ Module SayMyName
         Dim userMessage As String
         Console.WriteLine("Please enter your name. Press Enter when done.")
         userMessage = Console.ReadLine()
-        Console.WriteLine("You entered " & userMessage & ", welcome " & userMessage & ".")
+
+        If userMessage = "Doyle" Then
+            Console.WriteLine("Welcome back Tigerfang. Ready to continue?")
+        ElseIf userMessage = "Joe" Then
+            Console.WriteLine("Welcome  back user " & userMessage & ".")
+        ElseIf userMessage = "Emily" Then
+            Console.WriteLine("Welcome back user " & userMessage & ".")
+        Else
+            Console.WriteLine("Access is Denied.")
+        End If
+
         Console.ReadLine()
 
     End Sub
